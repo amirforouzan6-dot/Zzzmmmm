3786db446821b7ab3e803be6e502e402195a156b
amirforouzan6-dot <amirforouzan6@gmail.com>
2026-06-18 12:01:41 +0330

    bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 genai.configure(api_key=GEMINI_API_KEY)
 
-model = genai.GenerativeModel("gemini-1.5-pro")
+model = genai.GenerativeModel("gemini-2.0-flash")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("🤖 ربات 🇮🇷forozan🇮🇷 آماده است")

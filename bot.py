5753a95fce2ef1aeae86d26c4ae59946fe4e45d2
amirforouzan6-dot <amirforouzan6@gmail.com>
2026-06-18 14:26:56 +0330

    bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,95 @@
 import os
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
+import json
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    CallbackQueryHandler,
+    ContextTypes
+)
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 CREATOR = "امیر علی فروزان اصل"
+DB = "database.json"
+
+
+def load_db():
+    try:
+        with open(DB, "r", encoding="utf-8") as f:
+            return json.load(f)
+    except:
+        return {"users": {}}
+
+
+def save_db(data):
+    with open(DB, "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    keyboard = [
+        [
+            InlineKeyboardButton("✅ قبول قوانین", callback_data="accept"),
+            InlineKeyboardButton("❌ رد", callback_data="reject")
+        ]
+    ]
+
     await update.message.reply_text(
-        f"🇮🇷❄️ سلام، خوش آمدید\n\n"
-        f"سازنده: {CREATOR}"
+        f"""
+📜 قوانین ربات
+
+👨‍💻 سازنده: {CREATOR}
+
+قبل از استفاده قوانین را قبول کنید.
+
+مسئولیت استفاده با کاربر است.
+""",
+        reply_markup=InlineKeyboardMarkup(keyboard)
     )
 
+
+async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    query = update.callback_query
+    await query.answer()
+
+    user_id = str(query.from_user.id)
+
+    db = load_db()
+
+    if query.data == "accept":
+
+        db["users"][user_id] = {
+            "accepted": True,
+            "warnings": 0
+        }
+
+        save_db(db)
+
+        await query.edit_message_text(
+            "✅ قوانین قبول شد\n🤖 حالا می‌توانید از ربات استفاده کنید"
+        )
+
+
+    elif query.data == "reject":
+
+        await query.edit_message_text(
+            "❌ بدون قبول قوانین امکان استفاده نیست"
+        )
+
+
 def main():
+
     app = Application.builder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(CallbackQueryHandler(button))
 
-    print("Bot is running...")
+    print("Running...")
     app.run_polling()
 
+
 if __name__ == "__main__":
-    main()ontent)
+    main()

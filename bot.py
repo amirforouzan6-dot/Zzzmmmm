f13cc19a2619fd540f033e8847e972e29d1effae
amirforouzan6-dot <amirforouzan6@gmail.com>
2026-06-18 11:45:39 +0330

    bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,3 @@
-try:
-    response = model.generate_content("سلام")
-    print("Gemini OK")
-except Exception as e:
-    print("Gemini Failed:", e)
-
 import os
 import google.generativeai as genai
 
@@ -29,9 +23,9 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 genai.configure(api_key=GEMINI_API_KEY)
 
-model = genai.GenerativeModel("gemini-2.0-flash")
-    
-
+model = genai.GenerativeModel(
+    "gemini-1.5-flash"
+)
 
 RULES = """
 📜 قوانین ربات

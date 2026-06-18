63dae3c9263ef5e3170aad2641402bf731319f60
amirforouzan6-dot <amirforouzan6@gmail.com>
2026-06-18 11:24:37 +0330

    bot.py

--- bot.py
+++ bot.py
@@ -24,7 +24,7 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=GEMINI_API_KEY)
 
 model = genai.GenerativeModel(
-    "gemini-1.5-flash"
+    "gemini-1.5-pro"
 )
 
 RULES = """
